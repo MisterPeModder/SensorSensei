e83b64173de8fece6e9fd13983e6d94423a395f9
Yanis Guaye <yanis.guaye@epitech.eu>
2025-02-27 17:09:36 +0100

    feat(1): add sensor board intitial Rust project

---
+++ sensor/src/bin/main.rs
@@ -0,0 +1,22 @@
+#![no_std]
+#![no_main]
+
+use esp_backtrace as _;
+use esp_hal::{clock::CpuClock, delay::Delay, main};
+use log::info;
+
+#[main]
+fn main() -> ! {
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let _peripherals = esp_hal::init(config);
+
+    esp_println::logger::init_logger_from_env();
+
+    let delay = Delay::new();
+    let mut cycle: u32 = 0;
+    loop {
+        cycle += 1;
+        info!("Hello world! x{cycle}");
+        delay.delay_millis(500);
+    }
+}
