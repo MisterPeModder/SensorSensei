505b046dbdcb1ce9ac93edaaf58c08b9557f8b1c
Andreas Leroux <andreas.leroux@epitech.eu>
2025-04-04 11:47:01 +0200

    feat(27): setup lora-rs for sensor board

--- sensor-board/src/bin/main.rs
+++ sensor-board/src/bin/main.rs
@@ -1,22 +1,47 @@
+//! This example runs on the Heltec WiFi LoRa ESP32 board, which has a builtin Semtech Sx1276 radio.
+//! It demonstrates LORA P2P send functionality.
 #![no_std]
 #![no_main]
 
-use esp_backtrace as _;
-use esp_hal::{clock::CpuClock, delay::Delay, main};
-use log::info;
+use defmt::info;
+use embassy_executor::Spawner;
+use esp_hal::{clock::CpuClock, timer::timg::TimerGroup};
+use esp_println as _;
+use sensor_board::lora::{LoraController, LoraHardware};
 
-#[main]
-fn main() -> ! {
-    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let _peripherals = esp_hal::init(config);
+#[esp_hal_embassy::main]
+async fn main(_spawner: Spawner) {
+    info!("Starting LoRa P2P TX example");
+    // Set up ESP32
+    let peripherals = esp_hal::init(esp_hal::Config::default().with_cpu_clock(CpuClock::max()));
+    let timer_group = TimerGroup::new(peripherals.TIMG0);
+    esp_hal_embassy::init(timer_group.timer1);
 
-    esp_println::logger::init_logger_from_env();
+    let mut lora = LoraController::new(LoraHardware {
+        spi: peripherals.SPI2,
+        spi_nss: peripherals.GPIO18,
+        spi_scl: peripherals.GPIO5,
+        spi_mosi: peripherals.GPIO27,
+        spi_miso: peripherals.GPIO19,
+        reset: peripherals.GPIO23,
+        dio1: peripherals.GPIO26,
+    })
+    .await
+    .unwrap();
+    let buffer = b"world";
 
-    let delay = Delay::new();
-    let mut cycle: u32 = 0;
     loop {
-        cycle += 1;
-        info!("Hello from Sensor Board! x{cycle}");
-        delay.delay_millis(500);
+        // for i in 0..10 {
+        info!("Sending packet {}", buffer);
+        lora.send(buffer).await.unwrap();
+        // sleep
+        info!("Sleeping for 1 second");
+        embassy_time::Timer::after(embassy_time::Duration::from_secs(1)).await;
     }
+}
+
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    defmt::info!("Panic: {}", info);
+    loop {}
 }
---
+++ sensor-board/src/lora.rs
@@ -0,0 +1,154 @@
+use defmt::{error, info, Format};
+use embassy_embedded_hal::shared_bus::asynch::spi::SpiDevice;
+use embassy_sync::{blocking_mutex::raw::NoopRawMutex, mutex::Mutex};
+use embassy_time::Delay;
+use esp_hal::{
+    gpio::{GpioPin, Input, InputConfig, Level, Output, OutputConfig},
+    peripherals::SPI2,
+    spi::Mode,
+    time::Rate,
+    Async,
+};
+use lora_phy::{
+    iv::GenericSx127xInterfaceVariant,
+    mod_params::{
+        Bandwidth, CodingRate, ModulationParams, PacketParams, RadioError, SpreadingFactor,
+    },
+    sx127x::{self, Sx1276, Sx127x},
+    LoRa,
+};
+use static_cell::StaticCell;
+use thiserror::Error;
+
+/// Channel to use, should be "unique". Use same frequencies as other devices causes spurious packets.
+const LORA_FREQUENCY_IN_HZ: u32 = 868_200_000;
+/// Channel width. Lower values increase time on air, but may be able to find clear frequencies.
+const LORA_BANDWITH: Bandwidth = Bandwidth::_250KHz;
+/// Controls the forward error correction. Higher values are more robust, but reduces the ratio
+/// of actual data in transmissions.
+const LORA_CODING_RATE: CodingRate = CodingRate::_4_8;
+/// Controls the chirp rate. Lower values are slower bandwidth (longer time on air), but more robust.
+const LORA_SPREADING_FACTOR: SpreadingFactor = SpreadingFactor::_10;
+
+pub struct LoraHardware {
+    pub spi: SPI2,
+    pub spi_nss: GpioPin<18>,
+    pub spi_scl: GpioPin<5>,
+    pub spi_mosi: GpioPin<27>,
+    pub spi_miso: GpioPin<19>,
+    pub reset: GpioPin<23>,
+    pub dio1: GpioPin<26>,
+}
+
+type AsyncSpi = esp_hal::spi::master::Spi<'static, Async>;
+type TBeamLora32Iv = GenericSx127xInterfaceVariant<Output<'static>, Input<'static>>;
+type TBeamLora32Lora = LoRa<
+    Sx127x<SpiDevice<'static, NoopRawMutex, AsyncSpi, Output<'static>>, TBeamLora32Iv, Sx1276>,
+    Delay,
+>;
+
+pub struct LoraController {
+    lora: TBeamLora32Lora,
+    modulation_params: ModulationParams,
+    tx_packet_params: PacketParams,
+}
+
+/// One-stop shop for LoRa-related errors
+#[derive(Debug, Error, Format)]
+pub enum LoraError {
+    #[error(transparent)]
+    Config(#[from] esp_hal::spi::master::ConfigError),
+    #[error("radio error: {0:?}")]
+    Radio(RadioError),
+}
+
+impl From<RadioError> for LoraError {
+    fn from(value: RadioError) -> Self {
+        LoraError::Radio(value)
+    }
+}
+
+static SPI_BUS: StaticCell<Mutex<NoopRawMutex, AsyncSpi>> = StaticCell::new();
+
+impl LoraController {
+    pub async fn new(hardware: LoraHardware) -> Result<Self, LoraError> {
+        // The SPI bus used by the lora dio is exclusive to it.
+        // No need to use mutexes or other synchonization
+        let spi: AsyncSpi = esp_hal::spi::master::Spi::new(
+            hardware.spi,
+            esp_hal::spi::master::Config::default()
+                .with_frequency(Rate::from_khz(100))
+                .with_mode(Mode::_0),
+        )?
+        .with_sck(hardware.spi_scl)
+        .with_mosi(hardware.spi_mosi)
+        .with_miso(hardware.spi_miso)
+        .into_async();
+
+        // Create the SX126x configuration
+        let sx127x_config = sx127x::Config {
+            chip: Sx1276,
+            tcxo_used: false,
+            tx_boost: true,
+            rx_boost: true,
+        };
+
+        // Initialize GPIO pins
+        let nss = Output::new(hardware.spi_nss, Level::High, OutputConfig::default());
+        let reset = Output::new(hardware.reset, Level::Low, OutputConfig::default());
+        let dio1 = Input::new(hardware.dio1, InputConfig::default());
+
+        // Initialize the SPI bus
+        let spi_bus: &mut Mutex<NoopRawMutex, AsyncSpi> = SPI_BUS.init_with(|| Mutex::new(spi));
+        let spi_device: SpiDevice<
+            '_,
+            NoopRawMutex,
+            esp_hal::spi::master::Spi<'_, Async>,
+            Output<'static>,
+        > = SpiDevice::new(spi_bus, nss);
+
+        // Create the radio instance
+        let iv: GenericSx127xInterfaceVariant<Output<'static>, Input<'static>> =
+            GenericSx127xInterfaceVariant::new(reset, dio1, None, None).unwrap();
+        let mut lora: TBeamLora32Lora =
+            LoRa::new(Sx127x::new(spi_device, iv, sx127x_config), false, Delay)
+                .await
+                .unwrap();
+
+        let modulation_params = lora.create_modulation_params(
+            LORA_SPREADING_FACTOR,
+            LORA_BANDWITH,
+            LORA_CODING_RATE,
+            LORA_FREQUENCY_IN_HZ,
+        )?;
+
+        // Don't ask: I don't know what that is either
+        let tx_packet_params =
+            lora.create_tx_packet_params(4, false, true, false, &modulation_params)?;
+
+        Ok(LoraController {
+            lora,
+            modulation_params,
+            tx_packet_params,
+        })
+    }
+
+    pub async fn send(&mut self, buffer: &[u8]) -> Result<(), LoraError> {
+        info!("Preparing for TX");
+        self.lora
+            .prepare_for_tx(
+                &self.modulation_params,
+                &mut self.tx_packet_params,
+                20,
+                buffer,
+            )
+            .await?;
+
+        info!("Send TX");
+        Ok(self.lora.tx().await?)
+    }
+
+    pub async fn sleep(&mut self, wakeup: bool) -> Result<(), LoraError> {
+        Ok(self.lora.sleep(wakeup).await?)
+    }
+}
