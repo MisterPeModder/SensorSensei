9d4c5112f94d57472391e86aa218c1370d26155c
Yanis Guaye <yanis.guaye@epitech.eu>
2025-06-12 17:39:36 +0200

    feat(21): add request handling API for the HTTP server

--- gateway-board/src/net/http/mod.rs
+++ gateway-board/src/net/http/mod.rs
@@ -10,5 +10,27 @@ const SOCKET_TIMEOUT: Duration = Duration::from_secs(10);
 
 #[derive(Clone, Copy)]
 pub enum HttpMethod {
+    Get,
     Post,
+}
+
+impl AsRef<str> for HttpMethod {
+    fn as_ref(&self) -> &str {
+        match self {
+            HttpMethod::Get => "GET",
+            HttpMethod::Post => "POST",
+        }
+    }
+}
+
+impl TryFrom<&[u8]> for HttpMethod {
+    type Error = ();
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        match value {
+            b"GET" => Ok(HttpMethod::Get),
+            b"POST" => Ok(HttpMethod::Post),
+            _ => Err(()),
+        }
+    }
 }
